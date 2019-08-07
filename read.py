ff5fa79ef51ef54aaba647ebe7af9b959cc904b3
likangkang08 <1712340552@qq.com>
2019-08-07 19:10:24 +0800

    增加了一些功能

---
+++ read.py
@@ -0,0 +1,37 @@
+from con_sql import Sql
+import os
+
+
+class ReadFile(object):
+    files_path = Sql().get_file_path()
+    file_path = None
+
+    def check_path(self, path):
+        #检查路径是否存在，不在返回1，存在返回路径。
+        if os.path.exists(path):
+            return path
+        else:
+            return 1
+
+    def read_file(self):
+        count =0
+        for path in self.files_path:
+            self.file_path = self.files_path[path]
+            if self.check_path(self.file_path) != 1:
+                # 统计行数
+                try:
+                   for line in open(self.file_path,'r',encoding='UTF-8'):
+                        count += 1
+
+                except Exception as e:
+                    print(e)
+                print("行数为：==========={}\n".format(count))
+                #打印内容
+                try:
+                    with open(self.file_path, 'r+',encoding='UTF-8')as file:
+                        file.read()
+                except Exception as e:
+                    print(e)
+
+
+ReadFile().read_file()
--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import psutil
 import os
 from sys import argv
+from con_sql import Sql
 
 
 class Search():
@@ -10,6 +11,22 @@ class Search():
     search_dir_name = None
     search_disc_name = None
     search_disc = None
+    sql_path = Sql().get_file_path()  # dict
+
+    def check_path(self, paths):
+        # 如果不相等继续比较，相等的话跳出循环，并且赋值。
+        flag = False
+        if len(self.sql_path) == 0:
+            flag = True
+        else:
+            for path in self.sql_path:
+                if self.sql_path[path] == paths:
+                    flag = False
+                    print("数据库有该值")
+                    break
+                else:
+                    flag = True
+        return flag
 
     def disc_name(self):
         # 判断要搜索的盘符是否存在，disc是要传入的参数。
@@ -49,8 +66,12 @@ class Search():
                 for filename in filenames:
                     file_path = os.path.join(parent, filename)
                     if filename == self.search_file_name:
-                        print(filename)
-                        print(file_path, '\n')
+                        if self.check_path(file_path):
+                            try:
+                                Sql().write_file(file_name=filename, file_path=file_path)
+                            except Exception as e:
+                                print("提交数据库失败 {}文件名：{}文件路径：{}".format(e, filename, file_path))
+
             print("指定盘符：{} 搜索完毕".format(self.search_disc_name))
 
 
